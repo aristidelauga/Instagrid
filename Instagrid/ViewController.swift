c59cb3bcb61c23f4d6846ca48d2d9266dd7e7ce0
aristidelauga <aristide.lauga@gmail.com>
2024-04-06 19:32:52 +0200

    Swipe view updating according to orientation

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -9,10 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+	@IBOutlet weak var SwipeUpLabel: UITextView!
+	@IBOutlet weak var arrowView: UIImageView!
+
+	override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+		NotificationCenter.default.addObserver(self, selector: #selector(orientationObserved), name: UIDevice.orientationDidChangeNotification, object: nil)
   }
+
+	@objc func orientationObserved() {
+		let orientation = UIDevice.current.orientation
+		if orientation.isPortrait {
+			SwipeUpLabel.text = "Swipe up to share"
+			arrowView.transform = CGAffineTransform(rotationAngle: 0)
+		} else {
+			SwipeUpLabel.text = "Swipe left to share"
+			arrowView.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi / 2))
+		}
+	}
 
  
 }
